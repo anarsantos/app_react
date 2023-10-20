25d6cd93b15e7fd34f8b1600f819c6c1d8c52a45
anarsantos <aninha_rsantos@yahoo.com.br>
2023-10-20 20:49:17 -0300

    Uso de props

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Page from './views/Page';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Page empresa='Webdesign' /> 
+      <Page empresa='Programação' />
+      <Page empresa='Devs' />     
     </View>
   );
 }
---
+++ views/Page.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function Page(props) 
+{
+    return (
+        <View>
+            <Text>O nome da empresa é {props.empresa}</Text>
+        </View>
+    );
+}
